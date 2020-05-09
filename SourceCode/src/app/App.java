4f5a4638995d6eeaf79f0f974ba041c6383ffb3f
Harshit Mahajan <hmahajan1999@gmail.com>
2020-05-09 17:10:08 +0530

    Tested code

--- SourceCode/src/app/App.java
+++ SourceCode/src/app/App.java
@@ -1,23 +1,46 @@
 package app;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
+        test();
+    }
+
+    public static void test() {
         System.out.println("Hello Java");
+        testEmployee();
+        testContractualEmployee();
+        testSalariedEmployee();
+    }
 
-        SalariedEmployee se = new SalariedEmployee(12, "name");
-        System.out.println(se.basicDetails().getId());
-        se.basicDetails().setName("S-EMP");
-        System.out.println(se.basicDetails().getName());
-        System.out.println(se.basicDetails().getPaymentMethod());
+    public static void testEmployee() {
+        System.out.println("-----------------------");
+        Employee e = new Employee(0,"");
+        System.out.println(e.getId());
+        e.setName("Basic Emloyee");
+        System.out.println(e.getName());
+        System.out.println(e.getPaymentMethod());
+        System.out.println("-----------------------");
+    }
 
-        ContractualEmployee ce = new ContractualEmployee(12, "name");
+    public static void testContractualEmployee() {
+        System.out.println("-----------------------");
+        ContractualEmployee ce = new ContractualEmployee(1, "name");
         System.out.println(ce.basicDetails().getId());
-        ce.basicDetails().setName("C-EMP");
+        ce.basicDetails().setName("Contractual Employee");
         System.out.println(ce.basicDetails().getName());
         System.out.println(ce.basicDetails().getPaymentMethod());
+        System.out.println("-----------------------");
+    }
 
-
-
+    public static void testSalariedEmployee() {
+        System.out.println("-----------------------");
+        SalariedEmployee se = new SalariedEmployee(2, "name");
+        System.out.println(se.basicDetails().getId());
+        se.basicDetails().setName("Salaried Employee");
+        System.out.println(se.basicDetails().getName());
+        System.out.println(se.basicDetails().getPaymentMethod());
+        System.out.println("-----------------------");
     }
 
 }
